d6d95227d63f740f624a16b5214ad6387b71b58d
Mark Miranda <mark.miranda@babylist.com>
2025-11-24 15:58:38 -0700

    Initial commit: Xcode project setup

---
+++ GolfDads/GolfDadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GolfDadsApp.swift
+//  GolfDads
+//
+//  Created by Mark Miranda on 11/24/25.
+//
+
+import SwiftUI
+
+@main
+struct GolfDadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GolfDadsTests/GolfDadsTests.swift
@@ -0,0 +1,17 @@
+//
+//  GolfDadsTests.swift
+//  GolfDadsTests
+//
+//  Created by Mark Miranda on 11/24/25.
+//
+
+import Testing
+@testable import GolfDads
+
+struct GolfDadsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
